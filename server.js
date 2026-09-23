0721517af8974e03bfa50badd6f4aee171f8a690
TuNombre <tu_correo@gmail.com>
2026-09-23 17:18:53 -0600

    Fix puerto Render

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/api/estado', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor Autónomo ejecutándose en puerto ${PORT}`);
 });
